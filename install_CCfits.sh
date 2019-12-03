d65c79f671a2546f63a4da9bc4f4e68cdd58041d
bessonj <jeremy.besson@l2s.centralesupelec.fr>
2019-12-03 15:50:47 +0100

    Ajout script installation CCfits et Cfitsio

---
+++ install_CCfits.sh
@@ -0,0 +1,18 @@
+mkdir ccfits_and_cfitsio
+cd ccfits_and_cfitsio
+wget https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.5.tar.gz
+wget heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3450.tar.gz
+tar zxvf CCfits-2.5.tar.gz
+tar zxvf cfitsio3450.tar.gz
+cd cfitsio
+./configure --prefix=/usr
+make
+sudo make install
+cd ..
+cd CCfits
+./configure --with-cfitsio=/usr/lib --prefix=/usr
+make 
+sudo make install
+cd ..
+cd ..
+rm -r ccfits_and_cfitsio
